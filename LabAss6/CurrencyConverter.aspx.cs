5bc5227ba16adf0e72624373a39d44f6b1656c39
Sun wake <2655077476@qq.com>
2023-04-28 15:18:16 +0800

    lab6

---
+++ LabAss6/CurrencyConverter.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LabAss6
+{
+    public partial class CurrencyConverter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int yuan = Convert.ToInt32(txtInput.Text.Trim());
+                int dollars = yuan * 6;
+                lblResult.Text = yuan + " Chinese Yuan = " + dollars + "Dollar.";
+            }catch(Exception ex)
+            {
+                lblResult.Text = "Input Error!Please enter the number!";
+            }
+
+        }
+    }
+}
